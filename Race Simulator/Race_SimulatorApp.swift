dab84e6996f3d6c650413adfb49afdeda41eef9a
Batuhan Kandiran <batuhankandiran@gmail.com>
2025-03-13 21:56:51 +0300

    Initial Commit

---
+++ Race Simulator/Race_SimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Race_SimulatorApp.swift
+//  Race Simulator
+//
+//  Created by Batuhan KANDIRAN on 13.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Race_SimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Race SimulatorTests/Race_SimulatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Race_SimulatorTests.swift
+//  Race SimulatorTests
+//
+//  Created by Batuhan KANDIRAN on 13.03.2025.
+//
+
+import Testing
+@testable import Race_Simulator
+
+struct Race_SimulatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
